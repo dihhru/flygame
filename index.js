b115aded9beca1b9b85733d97478c329573de148
dihhru <dihhru@mail.ru>
2020-02-29 00:58:47 +0300

    1
    1

--- index.js
+++ index.js
@@ -34,12 +34,49 @@ const sounds = [
     "D1",
     "C1"
   ], //Mozart
-  ["D2", "C2"]
+  ["D2", "G2", "G1", "B2", "D2", "D1", "G1", "A2", "B2", "G1", "G1", "G1"],
+  //Bach
+  [
+    "C1",
+    "C1",
+    "G2",
+    "E2",
+    "C2",
+    "D2",
+    "B2",
+    "G1",
+    "C2",
+    "D2",
+    "E2",
+    "D2",
+    "C1",
+    "C1",
+    "G2",
+    "E2",
+    "C2",
+    "D2",
+    "B2",
+    "G1",
+    "C2",
+    "B2",
+    "A2",
+    "G1"
+  ]
+  //Brahms
 ];
 class Game {
-  constructor() {}
+  constructor() {
+    this.level = 0;
+  }
   drawNotes() {
-    console.log(1);
+    let notes = sounds[this.level]; //slice or something
+    notes.map(function(note, index) {
+      let notes = document.getElementById("notes");
+      let img = document.createElement("img");
+      img.src = "images/note.png";
+      img.style.width = "50px";
+      notes.appendChild(img);
+    });
   }
 }
 let game = new Game();
