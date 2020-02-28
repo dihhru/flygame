62761691a03e6ef0b0243efd5c62379069ddb240
dihhru <dihhru@mail.ru>
2020-02-28 22:56:54 +0300

    1
    1

---
+++ index.js
@@ -0,0 +1,46 @@
+const sounds = [
+  [
+    "E2",
+    "D2#",
+    "E2",
+    "D2#",
+    "E2",
+    "B2",
+    "D2",
+    "C2",
+    "A2",
+    "C1",
+    "E1",
+    "A2",
+    "B2",
+    "E1",
+    "C2",
+    "B2",
+    "A2"
+  ], //Beethoven
+  [
+    "C1",
+    "C1",
+    "G1",
+    "G1",
+    "A2",
+    "A2",
+    "G1",
+    "F1",
+    "F1",
+    "E1",
+    "E1",
+    "D1",
+    "D1",
+    "C1"
+  ], //Mozart
+  ["D2", "C2"]
+];
+class Game {
+  constructor() {}
+  drawNotes() {
+    console.log(1);
+  }
+}
+let game = new Game();
+game.drawNotes();
