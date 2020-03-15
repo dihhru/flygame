b3b8c284f01106883b422964a7808cf2166d4fe7
dihhru <dihhru@mail.ru>
2020-03-15 22:42:40 +0300

    213
    123

--- classes/inputs.js
+++ classes/inputs.js
@@ -9,29 +9,25 @@ class InputHandler {
           plane.moveY("-");
           break;
         case 32:
-          controller.isStarted = !this.isStarted;
+          controller.isStarted = !controller.isStarted;
           break;
         case 13:
-          controller.isStarted = !this.isStarted;
+          controller.isStarted = !controller.isStarted;
           break;
         default:
           break;
       }
+      document.body.addEventListener(
+        "touchstart",
+        event => {
+          let height = document.documentElement.clientHeight;
+          let touchY = event.targetTouches[0].clientY;
+          let middle = height / 2;
+          touchY >= middle ? plane.moveY("-") : plane.moveY("+");
+          controller.isStarted = true;
+        },
+        false
+      );
     });
-  }
-}
-class TouchScreen {
-  constructor(plane, game) {
-    document.body.addEventListener(
-      "touchstart",
-      event => {
-        let height = document.documentElement.clientHeight;
-        let touchY = event.targetTouches[0].clientY;
-        let middle = height / 2;
-        touchY >= middle ? plane.moveY("-") : plane.moveY("+");
-        game.isStarted = true;
-      },
-      false
-    );
   }
 }
