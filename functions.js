b84304acc47f7fbf5fa3677b322588fd92c3bf04
dihhru <dihhru@mail.ru>
2020-03-16 19:08:35 +0300

    34
    345

--- functions.js
+++ functions.js
@@ -48,37 +48,7 @@ function throttle(func, ms) {
   }
   return wrapper;
 }
-let sum = 0;
-function loadAudio(resolve) {
-  let res = resolve;
-  let bg = document.getElementById("cords");
-  let length = uniq.length;
-  let i = 0;
-  let progress = document.getElementById("progressBar");
-  while (i < length) {
-    let sound = uniq[i];
-    let doc = document.createElement("audio");
-    doc.muted = true; //prevents "user didn't interact with the document first. err"
-    doc.src = `sounds/${sound}.wav`;
-    doc.id = "s" + i;
-    doc.muted = true;
-    doc.onloadeddata = function() {
-      console.log("loaded");
-      sum++;
-      console.log(sum);
-      progress.style.width = (100 / length) * sum + "%";
-    };
-    bg.appendChild(doc);
-    i++;
-  }
-  let timer = setInterval(() => {
-    if (sum === length) {
-      clearInterval(timer);
-      console.log("loaded audio");
-      res();
-    }
-  }, 500);
-}
+
 let game1 = throttle(gameLoop, 200);
 function start() {
   pannel.setPannel(0);
@@ -91,29 +61,82 @@ function gameLoop() {
   controller.update();
   requestAnimationFrame(gameLoop);
 }
-function loadImages() {
+function loadImages(resolve) {
+  let res = resolve;
   let bg = document.getElementById("res");
   let length = authors.length;
   let i = 0;
-  while (i < length) {
-    let author = authors[i];
-    let img = document.createElement("img");
-    img.id = author;
-    img.src = `images/authors/${author}.gif`;
-    img.width = "200";
-    img.height = "300";
-    bg.appendChild(img);
-    i++;
-  }
+  let sum = 0;
+  let allLength = length;
+  authors();
+  notes();
+  let timer = setInterval(() => {
+    console.log(sum);
+    if (sum >= allLength) {
+      clearInterval(timer);
+      console.log("loaded images");
+      res();
+    }
+  });
 
-  let i1 = 0;
-  while (i1 < 4) {
-    let img = document.createElement("img");
-    img.src = `images/notes/${i1}.png`;
-    img.width = "150";
-    img.id = i1;
-    bg.appendChild(img);
-    i1++;
+  function authors() {
+    while (i < length) {
+      let author = authors[i];
+      let img = document.createElement("img");
+      img.id = author;
+      img.src = `images/authors/${author}.gif`;
+      img.width = "200";
+      img.height = "300";
+      img.onloadeddata = () => sum++;
+      bg.appendChild(img);
+      i++;
+    }
+  }
+  function notes() {
+    let i1 = 0;
+    while (i1 < 4) {
+      let img = document.createElement("img");
+      img.src = `images/notes/${i1}.png`;
+      img.width = "150";
+      img.id = i1;
+      img.onloadeddata = () => sum++;
+      bg.appendChild(img);
+      i1++;
+    }
   }
 }
-//      <img id="2" src="images/notes/2.png" width="150" />
+
+function loadAudio(resolve) {
+  let res = resolve;
+  let bg = document.getElementById("cords");
+  let length = uniq.length;
+  let i = 0;
+  let progress = document.getElementById("progressBar");
+  let sum = 0;
+  audios();
+  let timer = setInterval(() => {
+    if (sum >= length) {
+      clearInterval(timer);
+      console.log("loaded audio");
+      res();
+    }
+  }, 500);
+  function audios() {
+    while (i < length) {
+      let sound = uniq[i];
+      let doc = document.createElement("audio");
+      doc.muted = true; //prevents "user didn't interact with the document first. err"
+      doc.src = `sounds/${sound}.wav`;
+      doc.id = "s" + i;
+      doc.muted = true;
+      doc.onloadeddata = function() {
+        console.log("loaded");
+        sum++;
+        console.log(sum);
+        progress.style.width = (100 / length) * sum + "%";
+      };
+      bg.appendChild(doc);
+      i++;
+    }
+  }
+}
--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const loading = document.getElementById("loading");
 sreenSize();
-let promise = new Promise(function(resolve) {
-  loadAudio(resolve);
-  loadImages();
-});
-promise.then(x => start());
 
+let promise1 = async function() {
+  let audio = await new Promise(resolve => loadAudio(resolve));
+  let images = await new Promise(resolve => loadImages(resolve));
+  return audio + images;
+};
+promise1().then(x => start());
 let plane = new Plane();
 let notes = new Notes(notesPositions);
 let pannel = new Pannel(plane, notes);
