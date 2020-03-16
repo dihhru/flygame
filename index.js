a683c9fd1161c34a791a034f4fc4268c6cf75d7e
dihhru <dihhru@mail.ru>
2020-03-16 22:06:13 +0300

    5
    5

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
+"use scrtict";
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const loading = document.getElementById("loading");
-sreenSize();
-let promise1 = async function() {
+const plane = new Plane();
+const notes = new Notes(notesPositions);
+const pannel = new Pannel(plane, notes);
+const controller = new Controller(pannel, plane, notes);
+const metro = new Object("metro", plane.setSpeed);
+const restart = new Object("restart", controller.startLvl);
+const input = new InputHandler(plane, controller);
+
+let prepare = async function() {
   let images, audio;
   let imagesP = new Promise((resolve, reject) => loadImages(resolve, reject));
   let audioP = new Promise((resolve, reject) => loadAudio(resolve, reject));
+  sreenSize();
+  controller.startLvl(0);
+  metro.create();
+  restart.create();
   images = await imagesP;
   audio = await audioP;
 };
-promise1()
+prepare()
   .then(x => start())
   .catch(e => alert(e));
-
-let plane = new Plane();
-let notes = new Notes(notesPositions);
-let pannel = new Pannel(plane, notes);
-let controller = new Controller(pannel, plane, notes);
-let metro = new Object("metro", plane.setSpeed);
-let restart = new Object("restart", controller.startLvl);
-let input = new InputHandler(plane, controller);
-controller.startLvl(0);
-metro.create();
-restart.create();
